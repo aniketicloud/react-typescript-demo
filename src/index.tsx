56423fdc3287bdb9f5003a100fabe803f2e7d230
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-01-03 23:12:45 +0530

    kept only simple child parent relationship

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import Parent from "./props/Parent";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Parent />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
