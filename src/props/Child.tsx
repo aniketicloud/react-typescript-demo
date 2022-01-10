c61082acec6a18997c8909e51f9811bb926ffdcb
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-11 01:09:44 +0530

    009-Annotations with children

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,13 +1,30 @@
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>Color from Child is {color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      Color from Child is {color}
+      <button onClick={onClick}>Click Child</button>
+    </div>
+  );
 };
 
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div> Color from React Child is {color}</div>;
+export const ChildAsFC: React.FC<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      Color from Child is {color}
+      <button onClick={onClick}>Click React.FC</button>
+      <br />
+      Children / Default slot passed from parent: {children}
+    </div>
+  );
 };
 
 ChildAsFC.displayName = "Component Display Name in Browser";
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -3,8 +3,10 @@ import { Child, ChildAsFC } from "./Child";
 const Parent = () => {
   return (
     <div>
-      <Child color="red" />
-      <ChildAsFC color="green" />
+      <Child color="red" onClick={() => console.log("Child")} />
+      <ChildAsFC color="green" onClick={() => console.log("React.FC Child")}>
+        some gibberish Default Slot Content or Children Content
+      </ChildAsFC>
     </div>
   );
 };
