86b8caa370a1507ad9fb02a83d6aec4ea5cc5c7e
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-12 21:17:12 +0530

    012-quick finish to example

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,11 +2,27 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState("");
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    if (name) {
+      setGuests([...guests, name]);
+      setName("");
+    }
+  };
+
   return (
     <div>
       <h3>Guest List</h3>
       <input value={name} onChange={(event) => setName(event.target.value)} />
-      <button>Add Guest</button>
+      <ul>
+        {guests.map((guest, index) => (
+          <li key={index}>
+            {index + 1} - {guest}
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClick}>Add Guest</button>
     </div>
   );
 };
