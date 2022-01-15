1ddf01052f08b81ae8786e5ae09371aa2e3253a6
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-15 23:11:06 +0530

    Testing Basics: testing user interactions and state

--- src/components/Greetings/Greetings.test.tsx
+++ src/components/Greetings/Greetings.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greetings from "./Greetings";
 describe("Greetings component", () => {
   test("renders 'Hello World!'", () => {
@@ -13,6 +14,49 @@ describe("Greetings component", () => {
     // !! `query...` functions will *NOT* throw an error if an element is not found
     // !! `find...` functions will return a promise
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
-    expect(helloWorldElement).toBeInTheDocument()
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'Original Text' at first", () => {
+    render(<Greetings />);
+    const originalText = screen.getByText("Original Text");
+    expect(originalText).toBeInTheDocument();
+  });
+
+  test("renders 'Text is changed' when the button is clicked", () => {
+    // ? Arrange
+    render(<Greetings />);
+
+    // ? Act = click the button
+    // !! `getBy...` will return a Single,
+    // !! `getByAll...` will return an Array,
+    // so in typescript, you get an error if you use the incorrect method
+    const buttonElement = screen.getByTestId("text-change-button");
+    userEvent.click(buttonElement);
+
+    // ? Assert = check for the changed text
+    const changedText = screen.getByText("Text is changed");
+    expect(changedText).toBeInTheDocument();
+  });
+
+  test("does not render 'Original Text' after clicking the button", () => {
+    // ? Copy Pasting and changing the above test
+
+    // ? Arrange
+    render(<Greetings />);
+
+    // ? Act = click the button
+    const buttonElement = screen.getByTestId("text-change-button");
+    userEvent.click(buttonElement);
+
+    // ? Assert = check if unavailability of the original text
+    // ! if you write the below code, the test will obviously fail,
+    // because you will not find the text with getByText method
+    // const originalText = screen.getByText("Original Text");
+    // expect(originalText).not.toBeInTheDocument();
+
+    // ! so, instead of using `get...` method, I am using `query...` method
+    const originalTextAvailibility = screen.queryByText("Original Text");
+    expect(originalTextAvailibility).toBeNull();
   });
 });
--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Greetings: React.FC = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const clickHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
+      {!changedText && <p>Original Text</p>}
+      {changedText && <p>Text is changed</p>}
+      <button onClick={clickHandler} data-testid="text-change-button">
+        Change the text
+      </button>
     </div>
   );
 };
