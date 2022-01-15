16e1cbc04cedd999be56a80ee71b32f494b7ab4d
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-15 23:18:27 +0530

    improv: function as prop from parent to child

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -3,7 +3,10 @@ import { Child, ChildAsFC } from "./Child";
 const Parent = () => {
   return (
     <div>
-      <Child color="red" onClick={() => console.log("Child")} />
+      <Child
+        color="red"
+        onClick={() => console.log("function as prop from parent to child")}
+      />
       <ChildAsFC color="green" onClick={() => console.log("React.FC Child")}>
         some gibberish Default Slot Content or Children Content
       </ChildAsFC>
