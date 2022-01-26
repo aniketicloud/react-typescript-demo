ffdc3e4b1e6f2d94b2b00b5ead1eaffd1078b08e
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-26 22:26:40 +0530

    updated element selector that is returning a promise
    add third parameter { timeout: number<ms> } if you have slow internet

--- src/components/Async/Async.test.tsx
+++ src/components/Async/Async.test.tsx
@@ -2,10 +2,10 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component", () => {
-  test("renders posts if request is successful", () => {
+  test("renders posts if request is successful", async () => {
     render(<Async />);
 
-    const listItemsElements = screen.getAllByRole("listitem");
+    const listItemsElements = await screen.findAllByRole("listitem");
     expect(listItemsElements).not.toHaveLength(0);
   });
 });
