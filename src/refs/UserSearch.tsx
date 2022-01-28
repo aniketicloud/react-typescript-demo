f50f81619e79cd393d3c665b1ae4631863e60acf
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-28 23:30:54 +0530

    019-start-applying types to refs

---
+++ src/refs/UserSearch.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export const users = [
+  { name: "aaa", age: 12 },
+  { name: "www", age: 12 },
+  { name: "zzz", age: 12 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [foundUser, setFoundUser] = useState<
+    undefined | { name: string; age: number }
+  >();
+  const [initialMessage, setInitialMessage] = useState("");
+
+  const onClick = () => {
+    setFoundUser(
+      users.find((user) => {
+        setInitialMessage("user not found");
+        return user.name === name;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <label htmlFor="searchedusername">Find the User:</label>
+      <input
+        type="search"
+        value={name}
+        id="searchedusername"
+        name="searchedusername"
+        aria-label="searchedusername"
+        onChange={(event) => setName(event.target.value)}
+      />
+      <button onClick={onClick}>Search</button>
+      <br />
+      {foundUser ? foundUser.name : initialMessage}
+    </div>
+  );
+};
+
+export default UserSearch;
