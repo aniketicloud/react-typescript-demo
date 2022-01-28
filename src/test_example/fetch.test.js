3b0cb00380826de0be6832b7cbfe47162aac6781
Aniket Pandharabale <aniketicloud@gmail.com>
2022-01-28 23:30:11 +0530

    skipped tests

--- src/test_example/fetch.test.js
+++ src/test_example/fetch.test.js
@@ -1,45 +1,49 @@
 // __tests__/fetch.test.js
 import React from 'react'
-import {rest} from 'msw'
-import {setupServer} from 'msw/node'
-import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Fetch from './fetch'
 
-const server = setupServer(
-  rest.get('/greeting', (req, res, ctx) => {
-    return res(ctx.json({greeting: 'hello there'}))
-  }),
-)
+describe.skip("Fetch component", () => {
 
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
-test('loads and displays greeting', async () => {
-  render(<Fetch url="/greeting" />)
-
-  fireEvent.click(screen.getByText('Load Greeting'))
-
-  await waitFor(() => screen.getByRole('heading'))
-
-  expect(screen.getByRole('heading')).toHaveTextContent('hello there')
-  expect(screen.getByRole('button')).toBeDisabled()
-})
-
-test('handles server error', async () => {
-  server.use(
+  const server = setupServer(
     rest.get('/greeting', (req, res, ctx) => {
-      return res(ctx.status(500))
+      return res(ctx.json({ greeting: 'hello there' }))
     }),
   )
 
-  render(<Fetch url="/greeting" />)
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
 
-  fireEvent.click(screen.getByText('Load Greeting'))
+  test('loads and displays greeting', async () => {
+    render(<Fetch url="/greeting" />)
 
-  await waitFor(() => screen.getByRole('alert'))
+    fireEvent.click(screen.getByText('Load Greeting'))
 
-  expect(screen.getByRole('alert')).toHaveTextContent('Oops, failed to fetch!')
-  expect(screen.getByRole('button')).not.toBeDisabled()
+    await waitFor(() => screen.getByRole('heading'))
+
+    expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+    expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  test('handles server error', async () => {
+    server.use(
+      rest.get('/greeting', (req, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    )
+
+    render(<Fetch url="/greeting" />)
+
+    fireEvent.click(screen.getByText('Load Greeting'))
+
+    await waitFor(() => screen.getByRole('alert'))
+
+    expect(screen.getByRole('alert')).toHaveTextContent('Oops, failed to fetch!')
+    expect(screen.getByRole('button')).not.toBeDisabled()
+  })
 })
